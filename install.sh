73726708d866e7bcc0e3a7c06d1ae820cedde2ba
YifanYang6 <yifanyang6@link.cuhk.edu.cn>
2025-11-06 14:09:10 +0800

    update helm
    Signed-off-by: YifanYang6 <yifanyang6@link.cuhk.edu.cn>

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@ sdk env install
 
 mvn clean install
 
+cd utilities/tools.descartes.teastore.dockerbase
+
+docker build -t 10.10.10.240/library/teastore-base:latest .
+
+
 skaffold build --default-repo=10.10.10.240/library
+
+
 
 helm install teastore examples/helm -n teastore --create-namespace
